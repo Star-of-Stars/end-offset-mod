18ddf3b86b89e399d689fb33ceec16f64915f4df
Star-of-Stars <1512438370@qq.com>
2026-01-01 22:19:21 +0800

    Rewrite generate method for EndGatewayFeature

--- src/main/java/com/example/endoffset/EndGatewayFeatureMixin.java
+++ src/main/java/com/example/endoffset/EndGatewayFeatureMixin.java
@@ -5,7 +5,8 @@ import net.minecraft.world.gen.feature.EndGatewayFeature;
 import net.minecraft.world.gen.feature.util.FeatureContext;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -40,18 +41,15 @@ public class EndGatewayFeatureMixin {
 
     @ModifyArg(
         method = "generate",
-        at = @At(
-         value = "INVOKE",
-            target =
-     "Lnet/minecraft/world/gen/feature/EndGatewayFeature;configure(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/world/gen/feature/EndGatewayFeature;"
-         ),
-        index = 0
-    )
-   private BlockPos offsetGatewayPosition(BlockPos originalPos) {
-        BlockPos targetPos = GATEWAY_MAP.get(originalPos);
+        at = @At("HEAD"),
+        cancellable = true
+        )
+    private void offsetGatewayPosition(FeatureContext context, CallbackInfoReturnable<Boolean> cir) {
+        BlockPos pos = context.getPos();
+        BlockPos targetPos = GATEWAY_MAP.get(pos);
         if (targetPos != null) {
-            return targetPos;
+            FeatureContext newContext = FeatureContext.create(targetPos);
+            cir.setReturnValue(((EndGatewayFeature)(Object)this).generate(newContext));
         }
-        return originalPos;
     }
 }
